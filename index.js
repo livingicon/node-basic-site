9bb49d024f4f355110c624906d4d4039547ea86a
Josh Hancock <livingicon@gmail.com>
2023-03-01 21:39:57 -0500

    Initialize npm

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+// Test
+// const nodeTest = (message) => console.log(message);
+// nodeTest('node is working');
 
-const http = require('http');
+const http = require('http'); // s?
 const url = require('url');
 const fs = require('fs');
 
-http.createServer((req, res) => { // 1. create a node server
-  // const q = url.parse(req.url, true);
-  // const filename = `. ${q.pathname}`;
-  // 2. get the requested url
-  const { pathname } = url.parse(req.url);
-  // 3. readFile
-  fs.readFile(filename, (err, data) => {
-    if(err) {
-      res.writeHead(404, {'Content-type': 'text/html'});
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+http
+  .createServer((req, res) => { // 1. create a node server
+    const q = url.parse(req.url, true); // 2. get the url by parsing
+    const filePath = `./${q.pathname}`;
+    console.log(filePath);
+    // 2. get the requested url
+    // 3. readFile
+    fs.readFile(filename, (err, data) => {
+      if(err) {
+        res.writeHead(404, {'Content-type': 'text/html'});
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
