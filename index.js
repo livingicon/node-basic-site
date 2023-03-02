18a56ce7700a9e1fb0e6732c324f68b398a14521
Josh Hancock <livingicon@gmail.com>
2023-03-01 22:42:18 -0500

    Add comments

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ const fs = require('fs');
 http
   .createServer((req, res) => { // 1. create a node server
     const q = url.parse(req.url, true); // 2. get the url by parsing
-    let filePath = `.${q.pathname === '/' ? '/index' : q.pathname}.html`;
+    let filePath = `.${q.pathname === '/' ? '/index' : q.pathname}.html`; // get dynamic filepath using parsed url pathname
       // console.log(filePath);
 
-    fs.readFile(filePath, (err, data) => {
+    fs.readFile(filePath, (err, data) => { // trigger listener (read the file for the filePath)
       console.log(filePath);
 
-      if (err) {
-        return fs.readFile('./404.html', (err, data) => {
+      if (err) { // if it doesn't load right
+        return fs.readFile('./404.html', (err, data) => { // return the error page
             res.writeHead(404, { "Content-Type": "text/html" });
             res.write(data);
             return res.end();
         })
       
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
+      } // else if it does read fine
+      res.writeHead(200, { "Content-Type": "text/html" }); // return the proper file page
       res.write(data);
       return res.end();
     });
   })
-  .listen(8080, () => console.log('Listening on port 8080...'));
+  .listen(8080, () => console.log('Listening on port 8080...')); // create listener
