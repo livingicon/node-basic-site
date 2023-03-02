9ad0f22650df58c4f1354d4b566e00cc7fe1e131
Josh Hancock <livingicon@gmail.com>
2023-03-01 22:25:18 -0500

    Add js functionality

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ const fs = require('fs');
 http
   .createServer((req, res) => { // 1. create a node server
     const q = url.parse(req.url, true); // 2. get the url by parsing
-    const filePath = `./${q.pathname}`;
-    console.log(filePath);
-    // 2. get the requested url
-    // 3. readFile
-    fs.readFile(filename, (err, data) => {
-      if(err) {
-        res.writeHead(404, {'Content-type': 'text/html'});
-        return res.end("404 Not Found");
+    let filePath = `./${q.pathname === '/' ? 'index' : q.pathname}.html`;
+      console.log(filePath);
+
+    fs.readFile(filePath, (err, data) => {
+      console.log(filePath);
+
+      if (err) {
+        return fs.readFile('./404.html', (err, data) => {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write(data);
+            return res.end();
+        })
+      
       }
-      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
       return res.end();
     });
   })
-  .listen(8080);
+  .listen(8080, () => console.log('Listening on port 8080...'));
